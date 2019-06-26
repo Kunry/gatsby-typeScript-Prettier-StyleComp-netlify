045253fe7d29c7f986ec038b3d8e8b5d9d62790b
Kunry <kunry3@hotmail.com>
2019-06-26 16:09:54 +0200

    solved bug

--- src/components/PruebaParams.tsx
+++ src/components/PruebaParams.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 const PruebaParams = (props: any) => {
-  if (props.pageContext.id === "Prueba") {
-    props.navigate("/level1");
-  }
+  
   return <h1>Prueba Params</h1>;
 };
 export default PruebaParams;
