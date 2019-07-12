a84489aad67034689ae330f1817c592d003f4c48
Kunry <kunry3@hotmail.com>
2019-07-12 13:59:17 +0200

    p

--- functions/identity-login.js
+++ functions/identity-login.js
@@ -1,6 +1,6 @@
 exports.handler = function(event, context, callback) {
-    console.log(Object.keys(this.config));
-    console.log(Object.values(this.config));
+    console.log(Object.keys(this));
+    console.log(Object.values(this));
     callback(null, {
     statusCode: 500,
     body: "ERROR: NO PEPE"
