e2d2962b961a2ba16cdf09bac735ee5a41185af0
Kunry <kunry3@hotmail.com>
2019-07-12 12:48:03 +0200

    otra oportunidad

--- functions/identity-login.js
+++ functions/identity-login.js
@@ -1,8 +1,11 @@
 exports.handler = function(event, context, callback) {
-    console.log(event)
-    console.log(context)
-    console.log(context.done());
+    console.log(arguments)
+    // console.log(context)
+    // console.log(context.done());
+     const {identity, user} = context.clientContext;
+     console.log(identity, user)
     callback(null, {
-    statusCode: 200
+    statusCode: 500,
+    body:{error:{msg:"No pepe"}}
     });
 }
