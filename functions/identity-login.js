554c4a30d9f0f7980a270172e4d89dbb732b6a84
Kunry <kunry3@hotmail.com>
2019-07-12 12:00:26 +0200

    go

--- functions/identity-login.js
+++ functions/identity-login.js
@@ -1,9 +1,8 @@
 exports.handler = function(event, context, callback) {
     console.log(event)
     console.log(context)
-
+    console.log(context.done());
     callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
+    statusCode: 200
     });
 }
