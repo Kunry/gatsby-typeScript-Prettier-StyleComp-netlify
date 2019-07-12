1f084108730f3e0d68db4030a9959449e2c03426
Kunry <kunry3@hotmail.com>
2019-07-12 12:51:48 +0200

    failed

--- functions/identity-login.js
+++ functions/identity-login.js
@@ -4,8 +4,5 @@ exports.handler = function(event, context, callback) {
     // console.log(context.done());
      const {identity, user} = context.clientContext;
      console.log(identity, user)
-    callback(null, {
-    statusCode: 500,
-    body:{error:{msg:"No pepe"}}
-    });
+     context.fail("no pepe")
 }
