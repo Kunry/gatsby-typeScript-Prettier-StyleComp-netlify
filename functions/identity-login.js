741eeab4ad6f2aaa13b1fc28a2279b082d80772b
Kunry <kunry3@hotmail.com>
2019-07-15 10:14:27 +0200

    login process

--- functions/identity-login.js
+++ functions/identity-login.js
@@ -1,8 +1,5 @@
 exports.handler = function(event, context, callback) {
-    
-    console.log(Object.keys(this.process.config));
-    console.log(Object.keys(this.process.domain));
-    
+    console.log(Object.keys(typeof this.process));
     callback(null, {
     statusCode: 200
     });
