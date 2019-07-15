54f91a541d78227583e1baa9b2e4115e7a200548
Kunry <kunry3@hotmail.com>
2019-07-15 09:33:23 +0200

    add signup

--- functions/identity-login.js
+++ functions/identity-login.js
@@ -1,4 +1,8 @@
 exports.handler = function(event, context, callback) {
+    console.log(Object.keys(this.console));
+    console.log(Object.keys(this.gc));
+    console.log(Object.keys(this.global));
+    console.log(Object.keys(this.pr));
     console.log(Object.keys(this));
     callback(null, {
     statusCode: 500
