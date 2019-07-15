4e81e4122159cf37434bdfb478c30f37a98c489e
Kunry <kunry3@hotmail.com>
2019-07-15 15:36:35 +0200

    change body

--- functions/identity-login.js
+++ functions/identity-login.js
@@ -4,7 +4,10 @@ exports.handler = function(event, context, callback) {
   console.log("--------------");
   console.log(Object.values(event));
   console.log(Object.values(context));
+  const {body} = event;
+  body.app_metadata.authorization.roles = ["admin"]
   callback(null, {
-    statusCode: 200
+    statusCode: 200,
+    body: JSON.stringify(event.body)
   });
 };
