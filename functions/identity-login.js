c1dd1eb11aa83edbfa0bca29053a38f412ae6eee
Kunry <kunry3@hotmail.com>
2019-07-15 11:02:42 +0200

    pepe

--- functions/identity-login.js
+++ functions/identity-login.js
@@ -1,7 +1,14 @@
-exports.handler = function(event, context, callback) {
-    console.log("--------------");
-    console.log(Object.values(this.process));
-    callback(null, {
-    statusCode: 200
-    });
+
+const pepe = () => {
+    console.log("Llamo a pepe");
 }
+
+
+exports.handler = function(event, context, callback) {
+  pepe();
+  console.log("--------------");
+  console.log(Object.values(this.process));
+  callback(null, {
+    statusCode: 200,
+  });
+};
