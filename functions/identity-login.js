7f63fb822c3e9a43487cfdab84a41448102eb238
Kunry <kunry3@hotmail.com>
2019-07-15 11:33:38 +0200

    otra oportunidad

--- functions/identity-login.js
+++ functions/identity-login.js
@@ -10,7 +10,6 @@ exports.handler = function(event, context, callback) {
   console.log("--------------");
 //   console.log(Object.values(this.process));
   callback(null, {
-    statusCode: 500,
-    body: JSON.stringify({"msg": "NOP"})
+    statusCode: 200
   });
 };
