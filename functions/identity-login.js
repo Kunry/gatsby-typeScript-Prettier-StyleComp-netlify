423d4df5fac629043212f82f3c798ad429c8d9c7
Kunry <kunry3@hotmail.com>
2019-07-15 16:15:32 +0200

    errror

--- functions/identity-login.js
+++ functions/identity-login.js
@@ -3,9 +3,9 @@
 exports.handler = function(event, context, callback) {
   console.log("--------------");
   const {body} = event;
-  body.app_metadata.authorization.roles = ["admin"]
   console.log(body);
   callback(null, {
-    statusCode: 200
+    statusCode: 200,
+    body: JSON.stringify({"input": {"app_metadata": {"authorization": {"roles": ["admin"]}}}})
   });
 };
