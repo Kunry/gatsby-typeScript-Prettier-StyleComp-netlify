613722f2d0e661514dbaa8cc018ccb2ff8ceefc9
Kunry <kunry3@hotmail.com>
2019-07-15 17:28:17 +0200

    otra oportunidad

--- functions/identity-login.js
+++ functions/identity-login.js
@@ -1,6 +1,14 @@
+const Gotrue = require("gotrue-js");
 
+const auth = new Gotrue({
+  APIUrl: "https://sleepy-kepler-5e395e.netlify.com/.netlify/identity",
+  audience: "",
+  setCookie: false
+})
 
 exports.handler = function(event, context, callback) {
+  console.log("--------------");
+  console.log(auth.currentUser());
   console.log("--------------");
   const {body} = event;
   console.log(body);
