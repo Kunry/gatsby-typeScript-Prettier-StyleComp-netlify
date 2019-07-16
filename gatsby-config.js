7aa60abed52ca3f49bed4cb7e13c510dba4fda93
Kunry <kunry3@hotmail.com>
2019-07-16 16:28:06 +0200

    public

--- gatsby-config.js
+++ gatsby-config.js
@@ -101,7 +101,7 @@ module.exports = {
       resolve: "gatsby-plugin-netlify-cms",
       options: {
         modulePath: `${__dirname}/src/cms/cms.tsx`,
-        publicPath: `admin/test`,
+        publicPath: `admin/test/index.html`,
         enableIdentityWidget: false
       },
     },
