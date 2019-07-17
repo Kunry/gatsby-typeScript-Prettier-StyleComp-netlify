c763a278e6f0c0cb873bfdc47e8b311fd391d3a9
Kunry <kunry3@hotmail.com>
2019-07-17 15:58:32 +0200

    create roles?

--- functions/update.js
+++ functions/update.js
@@ -15,8 +15,7 @@ exports.handler = async (event, context) => {
         return fetch(userUrl, {
             method: "PUT",
             headers: { Authorization: adminAuthHeader },
-            body: JSON.stringify({ role:"admin", app_metadata: { roles: ["admin"], authorization: {roles: ["pepe"]
-              } } })
+            body: JSON.stringify( { app_metadata: { roles: [event.queryStringParameters.role] } } )
         })
         .then(res => res.json())
         .then(data => {
