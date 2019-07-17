69c20c5101a6bb211251a948bdd39a6109de222d
Kunry <kunry3@hotmail.com>
2019-07-17 12:34:28 +0200

    goooo

--- functions/update.js
+++ functions/update.js
@@ -15,7 +15,7 @@ exports.handler = async (event, context) => {
         return fetch(userUrl, {
             method: "PUT",
             headers: { Authorization: adminAuthHeader },
-            body: JSON.stringify({ app_metadata: { roles: ["admin"], authorization: {roles: ["admin", "pepe"]
+            body: JSON.stringify({ app_metadata: { roles: ["admin"], authorization: {roles: ["pepe"]
               } } })
         })
         .then(res => res.json())
