5c74f534835afecc45d723493d2ffdcf58fd0600
Kunry <kunry3@hotmail.com>
2019-07-19 11:55:11 +0200

    no change file test2/config.yml

--- gatsby-config.js
+++ gatsby-config.js
@@ -109,18 +109,7 @@ module.exports = {
       resolve: "gatsby-plugin-netlify-cms",
       options: {
         modulePath: `${__dirname}/src/cms/cms.tsx`,
-        publicPath: `admin/test2`,
-        enableIdentityWidget: true
-
-      },
-    },
-    {
-      resolve: "gatsby-plugin-netlify-cms",
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.tsx`,
-        publicPath: `admin/test3`,
-        enableIdentityWidget: true
-
+        publicPath: `admin/test2`
       },
     },
     // "gatsby-plugin-netlify", // make sure to keep it last in the array
