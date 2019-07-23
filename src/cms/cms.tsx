bc4f1ce44bc3e78d7564543334475d16af55a7d9
Kunry <kunry3@hotmail.com>
2019-07-23 13:58:39 +0200

    validate

--- src/cms/cms.tsx
+++ src/cms/cms.tsx
@@ -1,3 +1,4 @@
+import { List, Map } from "immutable";
 import CMS from "netlify-cms";
 import Prism from "prismjs";
 import React from "react";
@@ -207,23 +208,41 @@ CMS.registerWidget("customStringURL", StringControlURL, TestPreview);
 
 interface ControlObjProps {
   value: Map<string, any>;
-  field: Map<[], any>;
+  field: Map<string, any>;
   onChangeObject: any;
+  componentValidate: any;
 }
 
 // tslint:disable-next-line: max-classes-per-file
 class ObjControl extends React.Component<ControlObjProps, any> {
+  private componentValidate: any;
+
   public isValid = () => {
     const { value } = this.props;
+    this.validate();
     return true;
+  };
+  public validate = () => {
+    const { field } = this.props;
+    let fields = field.get("field") || field.get("fields");
+    fields = List.isList(fields) ? fields : List([fields]);
+    // tslint:disable-next-line: no-shadowed-variable
+    fields.forEach((field: any) => {
+      if (field.get("widget") === "hidden") {
+        return;
+      }
+      this.componentValidate[field.get("name")]();
+    });
   };
 
   public render() {
     // tslint:disable-next-line: no-console
     console.log(this.props);
-    const language = this.props.value.get("language") || "";
-    const primaryCategory = this.props.value.get("primaryCategory") || "";
-    this.props.onChangeObject("url", `${language}/${primaryCategory}`);
+    if (this.props.value) {
+      const language = this.props.value.get("language") || "";
+      const primaryCategory = this.props.value.get("primaryCategory") || "";
+      this.props.onChangeObject("url", `${language}/${primaryCategory}`);
+    }
     // const ObjWidget = this.ObjWidget;
     const ObjWidget = CMS.getWidget("object").control;
     return <ObjWidget {...this.props} />;
--- src/types.d.ts
+++ src/types.d.ts
@@ -2,3 +2,4 @@ declare module "*.svg";
 declare module "disqus-react";
 declare module "netlify-cms";
 declare module "prismjs";
+declare module "immutable";
